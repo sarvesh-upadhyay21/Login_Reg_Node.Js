6a8b45a57cb61c41a314711e9a4e75880f48fb08
Sarvesh-upadhayay <sarvesh.upadhayay@progenbusiness.com>
2023-01-20 22:58:50 +0530

    some chenge in credentials

--- config/config.js
+++ config/config.js
@@ -5,8 +5,8 @@ const config = {
     DATABASE: process.env.MONGODB_URI,
   },
   default: {
-    SECRET: process.env.SECRET,
-    DATABASE: process.env.MONGODB_URI,
+    SECRET: "mysecretkey",
+    DATABASE: "mongodb+srv://shubham:RENXF27CJYnjSlhK@cluster0.woodiza.mongodb.net/test",
   },
 };
 exports.get = function get(env) { return config[env] || config.default; };
